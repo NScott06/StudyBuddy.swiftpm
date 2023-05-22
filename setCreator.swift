39ac6741bc299a563f5283a48e436e6c30c1c988
James Howard <jamh885@gmail.com>
2023-05-22 14:36:20 -0500

    ADDED STORAGE HOPEFULLY IT WORKS ON NSCOTT END

--- setCreator.swift
+++ setCreator.swift
@@ -3,34 +3,37 @@ import SwiftUI
 struct setCreator: View {
     @State var term1:String = ""
     @State var definition1:String = ""
+    @State var pair45 = UserDefaults.standard.value(forKey: "pair") as? Data
     @State var pair:[pairs] = []
+//    @State var pair = UserDefaults.standard.data(forKey: "pair")
     var body: some View {
-        
-        VStack{
-            Text("Your Terms")
-            HStack{
-                Text("\(term1)")
-                Text("\(definition1)")
-            }
-            Group{
-                TextField("Enter your term", text: $term1)
-                TextField("Enter the definition for your term",text: $definition1)
-            }
-            .textFieldStyle(.roundedBorder)
-            .padding()
-            
-            Button(action: {
-//                if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
-//                    if let dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
+        if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
+            if var dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
+                VStack{
+                    Text("Your Terms")
+                    HStack{
+                        Text("\(term1)")
+                        Text("\(definition1)")
+                    }
+                    Group{
+                        TextField("Enter your term", text: $term1)
+                        TextField("Enter the definition for your term",text: $definition1)
+                    }
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                    
+                    Button(action: {
+                        //                if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
+                        //                    if let dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
                         
-                        pair.append(pairs(term2: term1, definition2: definition1))
+                        dataDecoded.append(pairs(term2: term1, definition2: definition1))
                         print("Here")
                         print("Here")
                         print("Here")
                         print("Here")
                         print(pair)
-                        if pair != [] {
-                            if let data = try? JSONEncoder().encode(pair) {
+                        if dataDecoded != [] {
+                            if let data = try? JSONEncoder().encode(dataDecoded) {
                                 UserDefaults.standard.set(data, forKey: "pair")
                             } else{
                                 print("UH OH FAILED")
@@ -38,13 +41,13 @@ struct setCreator: View {
                         } else {
                             print("add something")
                         }
-//                    }
-//                }
-            }, label: {
-                Image(systemName: "plus.circle")
-            })
-            
-            
+                        //                    }
+                        //                }
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                    })
+                }
+            }
         }
     }
 }
--- setView.swift
+++ setView.swift
@@ -6,12 +6,15 @@ struct setView: View {
     @State var definition1: String
     var body: some View {
         VStack{
-            flipcard(term1: "", definition1: "", flipped: false)
-                .frame(width: 300, height: 200)
+            VStack{ //switch this to Z-stack in order to next card(when its made)
+                flipcard(term1: "", definition1: "", flipped: false)
+                    .frame(width: 300, height: 200)
+            }
+            .onAppear(perform: {
+                term1 = UserDefaults.standard.string(forKey: "term") ?? ""
+                definition1 = UserDefaults.standard.string(forKey: "definition") ?? ""
+            })
+
         }
-        .onAppear(perform: {
-            term1 = UserDefaults.standard.string(forKey: "term") ?? ""
-            definition1 = UserDefaults.standard.string(forKey: "definition") ?? ""
-        }) 
     }
 }
