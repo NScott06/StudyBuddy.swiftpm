8c08f147036a6c0f1f0e4660e1bc3de43fbd409c
Nicholas Scott <nscott4525@stu.d214.org>
2023-05-24 14:54:12 -0500

    added ability to view terms created and saved, added flashcard color picker

--- flipcard.swift
+++ flipcard.swift
@@ -19,39 +19,35 @@ struct flipcard: View {
     
     
     var body: some View {
-        
-        if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
-            if var dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
-                
-                ForEach(dataDecoded, id: \.self) { pair2 in
+//        ScrollView{}
+
+            if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
+                if var dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
+                    ForEach(dataDecoded, id: \.self) { pair2 in
+//                        ScrollView{}
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 20)
+                                    .foregroundColor(self.flipped ? frontCardColor : backCardColor)
+                                    .padding()
+                                    .rotation3DEffect(self.flipped ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+                                    .animation(.default)
+                                    .onTapGesture {
+                                        self.flipped.toggle()
+                                    }
+                                Text(flipped ? pair2.term2 : pair2.definition2 )
+                                    .rotation3DEffect(self.flipped ? Angle(degrees: 0): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+                                    .animation(.default)
+                                    .onTapGesture {
+                                        self.flipped.toggle()
+                                    }
+                            }
+                        }
                     
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(self.flipped ? Color.red : Color.blue)
-                            .padding()
-                            .rotation3DEffect(self.flipped ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
-                            .animation(.default)
-                            .onTapGesture {
-                                self.flipped.toggle()
-                            }
-                        Text(flipped ? pair2.term2 : pair2.definition2 )
-                        //                        Text(flipped ? "hi" : "bye" )
-                            .rotation3DEffect(self.flipped ? Angle(degrees: 0): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
-                            .animation(.default)
-                            .onTapGesture {
-                                self.flipped.toggle()
-                            }
+                        ColorPicker("Change Back Flashcard Color", selection: $frontCardColor)
+                        ColorPicker("Change Front Flashcard Color", selection: $backCardColor)
+                        
                     }
                 }
-//                VStack{
-//                    Button {
-//                        dataDecoded.shuffle()
-//                    } label: {
-//                        Text("next")
-//                    }
-//                }
-
             }
         }
-    }
-}
+
--- setCreator.swift
+++ setCreator.swift
@@ -3,51 +3,61 @@ import SwiftUI
 struct setCreator: View {
     @State var term1:String = ""
     @State var definition1:String = ""
-    @State var pair45 = UserDefaults.standard.value(forKey: "pair") as? Data
     @State var pair:[pairs] = []
-//    @State var pair = UserDefaults.standard.data(forKey: "pair")
+    //    @State var pair = UserDefaults.standard.data(forKey: "pair")
     var body: some View {
         if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
             if var dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
-                VStack{
-                    Text("Your Terms")
-                    HStack{
-                        Text("\(term1)")
-                        Text("\(definition1)")
-                    }
-                    Group{
-                        TextField("Enter your term", text: $term1)
-                        TextField("Enter the definition for your term",text: $definition1)
-                    }
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
-                    
-                    Button(action: {
-                        //                if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
-                        //                    if let dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
-                        
-                        dataDecoded.append(pairs(term2: term1, definition2: definition1))
-                        print("Here")
-                        print("Here")
-                        print("Here")
-                        print("Here")
-                        print(pair)
-                        if dataDecoded != [] {
-                            if let data = try? JSONEncoder().encode(dataDecoded) {
-                                UserDefaults.standard.set(data, forKey: "pair")
-                            } else{
-                                print("UH OH FAILED")
-                            }
-                        } else {
-                            print("add something")
+                HStack{
+                    VStack{
+                        Text("Terms")
+                        ForEach(dataDecoded,id: \.self){ item in
+                            
+                            Text(item.term2)
                         }
-                        //                    }
-                        //                }
-                    }, label: {
-                        Image(systemName: "plus.circle")
-                    })
+                    }
+                        VStack{
+                            Text("Definitions")
+                        ForEach(dataDecoded,id: \.self){ item in
+                            
+                                Text(item.definition2)
+                            }
+                    }
                 }
+                Group{
+                    TextField("Enter your term", text: $term1)
+                    TextField("Enter the definition for your term",text: $definition1)
+                }
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                
+                Button(action: {
+                    //                if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
+                    //                    if let dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
+                    
+                    dataDecoded.append(pairs(term2: term1, definition2: definition1))
+                    print("Here")
+                    print("Here")
+                    print("Here")
+                    print("Here")
+                    print(dataDecoded)
+                    if dataDecoded != [] {
+                        if let data = try? JSONEncoder().encode(dataDecoded) {
+                            UserDefaults.standard.set(data, forKey: "pair")
+                        } else{
+                            print("UH OH FAILED")
+                        }
+                    } else {
+                        print("add something")
+                    }
+                    //                    }
+                    //                }
+                }, label: {
+                    Image(systemName: "plus.circle")
+                })
             }
         }
     }
 }
+
+
