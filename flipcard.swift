c7cb8fb9cb4dda9fb17c786ea6beb3e65334b4b0
Nicholas Scott <nscott4525@stu.d214.org>
2023-05-26 14:54:19 -0500

    added working scrollview, and foundation for going between displaying term or definition on top

--- flipcard.swift
+++ flipcard.swift
@@ -10,42 +10,60 @@ import SwiftUI
 
 struct flipcard: View {
     
-    @State var frontCardColor = Color.red
-    @State var backCardColor = Color.blue
+    @State var frontCardColor:Color =  Color.blue
+    @State var backCardColor:Color = Color.red
     @State var flipped = false // state variable used to update the card
+    @State var termOrDefinition = false
     @State var number23432 = 0
     
     
     var body: some View {
-//        ScrollView{}
-
             if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
                 if var dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
                     ForEach(dataDecoded, id: \.self) { pair2 in
-//                        ScrollView{}
-                            ZStack{
-                                RoundedRectangle(cornerRadius: 20)
-                                    .foregroundColor(self.flipped ? frontCardColor : backCardColor)
-                                    .padding()
-                                    .rotation3DEffect(self.flipped ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
-                                    .animation(.default)
-                                    .onTapGesture {
-                                        self.flipped.toggle()
-                                    }
-                                Text(flipped ? pair2.term2 : pair2.definition2 )
-                                    .rotation3DEffect(self.flipped ? Angle(degrees: 0): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
-                                    .animation(.default)
-                                    .onTapGesture {
-                                        self.flipped.toggle()
-                                    }
+                        ZStack{
+                            
+                            RoundedRectangle(cornerRadius: 20)
+                            
+                                .foregroundColor(self.flipped ? frontCardColor : backCardColor)
+                                .padding()
+                                .rotation3DEffect(self.flipped ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+                                .animation(.default)
+                                .onTapGesture {
+                                    self.flipped.toggle()
+                                }
+                            VStack{
+                                if termOrDefinition == false{
+                                    Text(flipped ? pair2.term2 : pair2.definition2 )
+                                }
+                                else{
+                                    Text(flipped ? pair2.definition2 : pair2.term2)
+                                }
+                            }
+                            .rotation3DEffect(self.flipped ? Angle(degrees: 0): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+                            .animation(.default)
+                            .onTapGesture {
+                                self.flipped.toggle()
                             }
                         }
-                    
-                        ColorPicker("Change Back Flashcard Color", selection: $frontCardColor)
-                        ColorPicker("Change Front Flashcard Color", selection: $backCardColor)
                         
                     }
+                    
+                    ColorPicker("Change Back Flashcard Color", selection: $frontCardColor)
+                    ColorPicker("Change Front Flashcard Color", selection: $backCardColor)
+                    //                    Button {
+                    //                        UserDefaults.standard.set(frontCardColor, forKey: "FrontCardColor")
+                    //                        UserDefaults.standard.set(backCardColor, forKey: "BackCardColor")
+                    //                        frontCardColor = UserDefaults.standard.Color(forKey: "FrontCardColor")
+                    //
+                    //                    } label: {
+                    //                        Text("Save Color")
+                    //                    }
+                    
+                    
                 }
             }
         }
+    }
+
 
--- setView.swift
+++ setView.swift
@@ -3,22 +3,24 @@ import SwiftUI
 struct setView: View {
     @State var term1:String
     @State var definition1: String
+    
     var body: some View {
         VStack{
             if let data = UserDefaults.standard.value(forKey: "pair") as? Data {
                 if var dataDecoded = try? JSONDecoder().decode([pairs].self, from: data){
-                    ZStack{ //switch this to Z-stack in order to next card(when its made)
-                        
-                       flipcard()
-                            .frame(width: 300, height: 200)
-                        Button {
-                            [dataDecoded].next(item: dataDecoded)
-                        } label: {
-                            Text("WORK")
+                    ScrollView{
+                        VStack{ //switch this to Z-stack in order to next card(when its made)
+                            
+                            flipcard()
+                                .frame(width: 300, height: 200)
+                            Button {
+                                [dataDecoded].next(item: dataDecoded)
+                            } label: {
+                                Text("WORK")
+                            }
+                            .offset(y: 200)
                         }
-                        .offset(y: 200)
-
-                        
+                        .frame(maxWidth: .infinity,maxHeight: .infinity)
                     }
                     .onAppear(perform: {
                         term1 = UserDefaults.standard.string(forKey: "term") ?? ""
